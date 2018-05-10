a875a5e96ec89d8ae0dab0dabd523d52e76d3c1a
Darkweld <Darkweld@users.noreply.github.com>
2018-05-10 06:43:26 -0400

    Title done

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -44,13 +44,15 @@ class TextRight extends React.Component{
 
 class MainTitle extends React.Component {
   render() {
+    console.log(this.props.bool);
     return(
       <div className = {style.bigTextHolder}>
-      <CSSTransition in = {this.props.bool}
-        classNames = {style.everestTitle}
-        timeout={300}
-        >
-        <h1 className = {style.everestTitle}>Everest</h1>
+      <CSSTransition in = {true} classNames = {style.scaleOffscreen} appear = {true} timeout={10000}
+       onEntered = {() => {this.props.sub()}}>
+        <h1 className = {style.everestTitle}>EVEREST</h1>
+      </CSSTransition>
+      <CSSTransition in = {this.props.bool} classNames = {style.fade} timeout = {2000}>
+        <h1 className = {style.subheadingTitle}>The tallest mountain</h1>
       </CSSTransition>
       </div>
     );
@@ -60,23 +62,22 @@ class MainTitle extends React.Component {
 class Main extends React.Component {
   constructor() {
     super();
-    this.state = {title: false};
-    this.tick = this.tick.bind(this);
+    this.state = {title: false, subHeading: false};
+    this.setSubheading = this.setSubheading.bind(this);
   }
-  componentDidMount() {
-    this.timer = setInterval(() => this.tick(), 5000);
-  }
-  tick() {
-    this.setState({title: !this.state.title});
-  }
-  componentWillUnmount() {
-    clearInterval(this.timer);
-  }
+
+setSubheading () {
+  this.setState({subHeading: true});
+}
+
   render() {
+    let title = (this.state.title) ? <MainTitle sub = {this.setSubheading} bool = {this.state.subHeading} /> : null;
+
 
     return(
       <div className = {style.wrapper}>
-        <MainTitle bool = {this.state.title}/>
+        <button onClick = {e => this.setState({title: !this.state.title, subHeading: false})} />
+        {title}
       </div>
     );
   }
