49164afb57d93343e3f8f7e9d56186018667be7e
Darkweld <Darkweld@users.noreply.github.com>
2018-05-24 07:03:33 -0400

    code cleanup

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,7 +4,21 @@ import { CSSTransition, TransitionGroup } from "react-transition-group";
 import style from "../css/main.css";
 import everestText from "../assets/text/everestText.js";
 
-
+function LearnMore () {
+  return (
+    <div className = {style.linkBox}>
+      <h1 className = {style.attrHeader}>Attributions</h1>
+      <h3 className = {style.attrSub}>Information</h3>
+      <a className = {style.attribution} href = "http://www.alanarnette.com/blog/2017/12/17/everest-by-the-numbers-2018-edition/">
+      Everest by the numbers: 2018 edition
+      </a>
+      <h3 className = {style.attrSub}>Background</h3>
+      <a className = {style.attribution} href = "https://vimeo.com/114789268">
+      {"Falling Snow Realistic Overlay Loop"}
+      </a>
+    </div>
+  );
+}
 
 
 class Appear extends React.Component {
@@ -44,42 +58,58 @@ class MainTitle extends React.Component {
 class Main extends React.Component {
   constructor() {
     super();
-    this.state = {title: false, slide: 0, textProgress: 1, maxSlides: everestText.length, currentText: []};
+    this.state = {title: false, slide: 0, textProgress: 1, started: false, finished: false, currentText: []};
     this.progressInterval = null;
     this.wheelUp = this.wheelUp.bind(this);
     this.textProgress = this.textProgress.bind(this);
 }
 
 wheelUp(e) {
-let [slide, title] = [this.state.slide, false];
+let s = this.state;
+let [slide, title, started, finished] = [s.slide, false, s.started, s.finished];
 
   if (e.deltaY < 0) {
-     if (this.state.title) {
-       if (slide + 1 < this.state.maxSlides) {
-         slide++;
-         title = false;
+       switch(true) {
+          case (s.title):
+            slide++;
+            title = false;
+            break;
+          case (slide === 0):
+            (started) ? slide++ : started = true;
+            break;
+          case (slide === 3):
+            title = true;
+            break;
+          case (slide + 1 < everestText.length):
+            slide++;
+            break;
+          case (slide + 1 === everestText.length):
+            if (!finished) finished = true;
+            break;
        }
-     } else {
-        if (slide === 3) {
-          title = true;
-        } else if (slide + 1 < this.state.maxSlides) slide++;
-     }
 
   } else {
-     if (this.state.title) {
-        title = false;
-     } else {
-       if (slide === 4) {
-         title = true;
-         slide--;
-       } else if (slide > 0) {
-         slide--;
-       }
-     }
+
+    switch(true) {
+      case (finished):
+        finished = false;
+        break;
+      case (slide === 0):
+        started = false;
+        break;
+      case (slide === 4):
+        title = true;
+        slide--;
+        break;
+       case (slide > 0):
+        slide--;
+        break;
+    }
+
   }
   clearInterval(this.progressInterval);
   this.progressInterval = setInterval(this.textProgress, 250);
-  this.setState({textProgress: 0, slide: slide, title: title});
+  this.setState({textProgress: 0, slide: slide, title: title, started: started, finished: finished});
 }
 
 textProgress() {
@@ -95,7 +125,7 @@ componentWillUnmount() {
   render() {
     let title = (this.state.title) ? <MainTitle sub = {this.setSubheading} bool = {this.state.subHeading} /> : null;
     let slide = everestText[this.state.slide];
-    let text = (!this.state.title) ? slide.text.split(" ").slice(0, this.state.textProgress)
+    let text = (!this.state.title && this.state.started && !this.state.finished) ? slide.text.split(" ").slice(0, this.state.textProgress)
     .map((v, i) => (slide.bold === v) ?
     <b className = {style.slideTextBold}><span className = {style.slideText} key = {i}>{" " + v}</span></b>
     : <span className = {style.slideText} key = {i}>{" " + v}</span>)
