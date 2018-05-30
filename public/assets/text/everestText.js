880c96a3a48ea7b6722a28a18aa77fc61a2ce526
Darkweld <Darkweld@users.noreply.github.com>
2018-05-30 04:49:16 -0400

    heroku deploy

--- public/assets/text/everestText.js
+++ public/assets/text/everestText.js
@@ -4,7 +4,7 @@ export default [
   {text: "Many travel from around the world to attempt the ascent", bold: "ascent"},
   {text: "Some never come back"},
   {text: "The first known summit of Everest was in 1953", bold: "1953"},
-  {text: "To date, 4,833 different people have summited Everest for a total of 8,306 summits", bold: "different"},
+  {text: "To date, 4,833 different people have summited Everest for a total of 8,306 summits"},
   {text: "Of those, 288 have perished", bold: "288"},
   {text: "Because of the difficulty and cost in removing the bodies at such high altitudes", bold: "difficulty"},
   {text: "Many of the fallen climbers still lie, naturally mummified by the freezing temperatures", bold: "mummified"},
